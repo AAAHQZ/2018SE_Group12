3045a792883528ca83437365d20750ac00320ccf
AAA <huangquanzhe@126.com>
2018-12-25 16:12:15 +0800

    del

--- SE12_Crawler/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .maoyanCrawler import MovieCrawler
-from .wrappedSQL import wrappedSQL
-
-__author__ = 'Huang "AAA" Quanzhe'
-__all__  = ["MovieCrawler", "wrappedSQL"]
-
-
--- test.py
+++
@@ -1,3 +0,0 @@
-from SE12_Crawler import *
-
-MovieCrawler(2015,1,2018,12)
